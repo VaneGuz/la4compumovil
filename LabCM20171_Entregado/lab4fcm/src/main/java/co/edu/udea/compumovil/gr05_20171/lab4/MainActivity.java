362de923640e7d038299d4d6fcd567c507f72a28
tatyajh <talejandra.jaramillo@gmail.com>
2017-05-21 21:52:12 -0500

    Login solo con correo y password

--- LabCM20171_Entregado/lab4fcm/src/main/java/co/edu/udea/compumovil/gr05_20171/lab4/MainActivity.java
+++ LabCM20171_Entregado/lab4fcm/src/main/java/co/edu/udea/compumovil/gr05_20171/lab4/MainActivity.java
@@ -1,13 +1,109 @@
 package co.edu.udea.compumovil.gr05_20171.lab4;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //defining view objects
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private Button buttonRegister;
+    private TextView textViewSignin;
+    private ProgressDialog progressDialog;
+
+
+    //defining firebaseauth object
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initializing firebase auth object
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //initializing views
+
+        progressDialog= new ProgressDialog(this);
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        buttonRegister = (Button) findViewById(R.id.buttonRegister);
+        textViewSignin = (TextView) findViewById(R.id.textViewSignin);
+
+
+        //attaching listener to button
+        buttonRegister.setOnClickListener(this);
+        textViewSignin.setOnClickListener(this);
+           }
+
+    private void registerUser(){
+
+        //getting email and password from edit texts
+        String email = editTextEmail.getText().toString().trim();
+        String password  = editTextPassword.getText().toString().trim();
+
+        //checking if email and passwords are empty
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this,"Ingrese el email",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this,"Ingrese la contraseña",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        //if the email and password are not empty
+        //displaying a progress dialog
+        progressDialog.setMessage("Registrando usuario...");
+        progressDialog.show();
+
+        //creating a new user
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //checking if success
+                        if(task.isSuccessful()){
+                            //display some message here
+                            Toast.makeText(MainActivity.this,"Registro correcto",Toast.LENGTH_LONG).show();
+                        }else{
+                            //display some message here
+                            Toast.makeText(MainActivity.this,"Usuario ya registrado",Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                });
+
     }
+
+    @Override
+    public void onClick(View view) {
+        if(view == buttonRegister){
+        registerUser();
+    }
+
+    if(view == textViewSignin){
+
+    }
+    }
+
 }
+
+
+
